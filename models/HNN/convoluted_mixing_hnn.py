60f0e0ffcf11a5be1009f8aca0d151550ca0585f
daxus4 <davideraffaelli4@gmail.com>
2025-06-12 20:07:09 +0200

    cnn

--- models/HNN/convoluted_mixing_hnn.py
+++ models/HNN/convoluted_mixing_hnn.py
@@ -5,6 +5,9 @@ from models.HNN.hnn import HNN
 
 
 class ConvolutedMixingHNN(nn.Module):
+    @staticmethod
+    def get_connections_for_convoluted_mixing_hnn(
+        nodes_to_edges_connections
     def __init__(
         self,
         num_nodes: int,
@@ -14,15 +17,26 @@ class ConvolutedMixingHNN(nn.Module):
         nodes_to_edges_connections: tuple,
         edges_to_triangles_connections: tuple,
         triangles_to_tetrahedra_connections: tuple,
+        num_convolutional_channels: int,
         num_classes: int,
     ):
         super(ConvolutedMixingHNN, self).__init__()
 
+        self.conv_layer_price_vol = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=num_convolutional_channels,
+                kernel_size=(1, 2),
+                stride=(1, 2),
+            ),
+            nn.ReLU(),
+        )
+
         self.hnn = HNN(  # questa HNN bisogna inizializzarla con i parametri dopo la convoluzione
-            num_nodes,
-            num_edges,
-            num_triangles,
-            num_tetrahedra,
+            num_nodes * num_convolutional_channels,
+            num_edges * num_convolutional_channels,
+            num_triangles * num_convolutional_channels,
+            num_tetrahedra * num_convolutional_channels,
             nodes_to_edges_connections,
             edges_to_triangles_connections,
             triangles_to_tetrahedra_connections,
